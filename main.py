7c8c6d90dc4e3dbdc588d42200c7d829567bb45a
Xuuyuan <nexfoun@gmail.com>
2024-11-26 21:53:30 +0800

    perf: 无法连接到tikuAdapter接口时跳过网络题库题目, 提交随机答案时从所有选项中选择, 增加URL过期及题目获取失败提示, 网络题库未找到答案跳过题目时进行提示 fix: 修复网络题库部分仅在单选题中提示正确率过高问题 style: 更改部分表达

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ headers_tiku = {"content-type": "application/json"}
 
 
 def main():
-    print('====Qingmakiller====')
+    print('====Qingmakiller 青马易战自动答题工具====')
     print('形势与政策 15 毛泽东思想和中国特色社会主义理论体系概论 7')
     print('思想道德与法治 8 马克思主义基本原理 9')
     print('中国近现代史纲要 10 习近平新时代中国特色社会主义思想概论 12')
@@ -62,23 +62,26 @@ def main():
     print('党史学习 127 学习四史 132')
     print('学习二十大 166 中华民族共同体意识 183')
     print('网络文明素养提升 184')
-    print('====================')
+    print('======================================')
 
-    cookie = get_cookie_from_url(input('请输入URL：'))
-    subjectId = input('请输入需要刷题的科目ID：')
-    now_times_str = input('请输入当前答题数（不输入默认为0）：')
-    now_right_times_str = input('请输入当前答对数（不输入默认为0）：')
+    cookie = get_cookie_from_url(input('请输入URL: '))
+    subjectId = input('请输入需要刷题的科目ID: ')
+    if subjectId == '':
+        print('# 未输入科目ID! ')
+        return
+    now_times_str = input('请输入当前答题数（不输入默认为0）: ')
+    now_right_times_str = input('请输入当前答对数（不输入默认为0）: ')
     now_times = int(now_times_str) if now_times_str != '' else 0
     now_right_times = int(now_right_times_str) if now_right_times_str != '' else 0
     now_right_rate = now_right_times / now_times if now_times != 0 else 0 # 计算当前正确率
 
-    target_times_str = input('请输入需要刷到的保底答对次数（保底答对次数=本次运行答对数-当前答对数，不输入默认为550）：')
+    target_times_str = input('请输入需要刷到的保底答对次数（保底答对次数=本次运行答对数-当前答对数, 不输入默认为550）: ')
     target_times = int(target_times_str) if target_times_str != '' else 550
 
-    target_right_rate_str = input('请输入需要刷题的保底正确率（不输入默认为0.6）：')
+    target_right_rate_str = input('请输入需要刷题的保底正确率（不输入默认为0.6）: ')
     target_right_rate = float(target_right_rate_str) if target_right_rate_str != '' else 0.6
 
-    max_right_rate_str = input('请输入需要刷题的上限正确率（不输入默认为0.9）：')
+    max_right_rate_str = input('请输入需要刷题的上限正确率（不输入默认为0.9）: ')
     max_right_rate = float(max_right_rate_str) if max_right_rate_str != '' else 0.9
 
     headers = {
@@ -114,7 +117,13 @@ def main():
     while now_right_times <= target_times or now_right_rate < target_right_rate: # 循环条件
         # 获取题目及选项
         req = requests.post(f'http://112.5.88.114:31101/yiban-web/stu/nextSubject.jhtml?_={gettime()}', headers=headers, data={'courseId': subjectId})
+        if 'document.location=\'/host_not_found_error\'' in req.text:
+            print('# 该URL已过期, 请根据指引重新获取URL! ')
+            break
         html = json.loads(req.text)
+        if 'uuid' not in html['data']:
+            print('# 题目获取失败! 返回信息: ', html)
+            break
         UUID = html['data']['uuid']
         now_question = text_format(decrypt(html['data']['nextSubject']['subDescript']))
         now_question_type = 1 if html['data']['nextSubject']['subType'] == '多选题' else 0
@@ -124,35 +133,40 @@ def main():
         print(f'{now_right_times}->{target_times}', decrypt(html['data']['nextSubject']['subDescript']), text_options)
         # 检测刷题题目
         if '刷题' in now_question or '请选择' in now_question:
-            print('# 检测到防刷题题目，自动跳过')
+            print('# 检测到防刷题题目, 自动跳过')
             continue
         if now_question in questions: # 题目已存在本地题库
             if now_right_rate > max_right_rate: # 正确率过高
-                print('# 该题目已存在本地题库中/正确率过高！ 正确答案为 ' + questions[now_question] + '，将自动提交随机答案！')
+                print('# 该题目已存在本地题库中/正确率过高!  正确答案为 ' + questions[now_question] + ', 将自动提交随机答案! ')
                 if now_question_type == 0:
-                    my_answer = options_list[random.randint(0,1)]
+                    my_answer = options_list[random.randint(0,len(options)-1)]
                 else:
-                    my_answer = options_list[random.randint(0,1)] + options_list[random.randint(2,3)]
+                    my_answer = ''.join(random.sample(options_list[:len(options)], 2))
             else:
-                print('# 该题目已存在本地题库中！ 正确答案为 ' + questions[now_question] + '，将自动提交！')
+                print('# 该题目已存在本地题库中!  正确答案为 ' + questions[now_question] + ', 将自动提交! ')
                 my_answer = questions[now_question]
-        else: # 题目不存在本地题库中，尝试从网络题库获取
+        else: # 题目不存在本地题库中, 尝试从网络题库获取
             # my_answer = ''
             force_choose = False
             if now_question_type == 0:
                 for d in range(len(options)):
-                    if '下都是' in options[d] or '上都是' in options[d]:
+                    if '下都是' in options[d] or '上都是' in options[d]: # 有出现对应文本的选项直接提交
                         my_answer = options_list[d]
                         force_choose = True
-            if now_right_rate > max_right_rate: # 正确率过高，随机提交答案
+            if now_right_rate > max_right_rate: # 正确率过高, 随机提交答案
                 if now_question_type == 0:
-                    my_answer = options_list[random.randint(0,1)]
-                    print('# 正确率过高，将自动提交随机答案！ (A-B)')
+                    my_answer = options_list[random.randint(0,len(options)-1)]
                 else:
-                    my_answer = options_list[random.randint(0,1)] + options_list[random.randint(2,3)]
+                    my_answer = ''.join(random.sample(options_list[:len(options)], 2))
+                print('# 正确率过高, 将自动提交随机答案! ')
                 force_choose = True
             elif not force_choose:
-                req_tiku = requests.post('http://localhost:8060/adapter-service/search', json={"question": now_question, "type": now_question_type, "options": options}, headers=headers_tiku)
+                try:
+                    req_tiku = requests.post('http://localhost:8060/adapter-service/search', json={"question": now_question, "type": now_question_type, "options": options}, headers=headers_tiku)
+                except requests.exceptions.ConnectionError:
+                    print('# 无法连接到tikuAdapter, 自动跳过本题, 请先启动tikuAdapter再运行本程序! ')
+                    time.sleep(random.randint(4,10))
+                    continue
                 res_tiku = json.loads(req_tiku.text)
                 my_answer = res_tiku['answer']['answerKeyText']
                 if now_question_type == 0 and len(my_answer) > 1: # 避免单选题提交多选
@@ -162,18 +176,19 @@ def main():
                 
                 if now_question_type == 1: # 多选题去重
                     my_answer = ''.join(set(my_answer))
-                print('# 网络题库搜索结果：' + my_answer)
+                print('# 网络题库搜索结果: ' + my_answer)
             if my_answer == '': # 网络题库未找到答案
-                if now_right_rate > max_right_rate: # 正确率高于保底正确率，随机提交
+                if now_right_rate > max_right_rate: # 正确率高于保底正确率, 随机提交
                     if now_question_type == 0:
-                        my_answer = options_list[random.randint(0,1)]
+                        my_answer = options_list[random.randint(0,len(options)-1)]
                     else:
-                        my_answer = options_list[random.randint(0,1)] + options_list[random.randint(2,3)]
+                        my_answer = ''.join(random.sample(options_list[:len(options)], 2))
                     time.sleep(1)
                 else:
+                    print('# 网络题库未找到答案, 本题跳过! ')
                     time.sleep(3)
                     continue
-        # 随机休眠，防止检测
+        # 随机休眠, 防止检测
         time.sleep(random.randint(4,10))
 
         # 提交答案
@@ -191,7 +206,7 @@ def main():
                 sheet.append(data_list)
                 workbook.save(f'青马易战_{subjectId}.xlsx')
                 questions[now_question] = my_answer
-            print(f'# 本题回答正确！ 当前提交次数 {run_times} 目标答对数 {target_times} 现答对数 {now_right_times} 现答题数 {now_times} 正确率 {now_right_rate * 100:.2f}%/{target_right_rate * 100}%')
+            print(f'# 本题回答正确!  当前提交次数 {run_times} 目标答对数 {target_times} 现答对数 {now_right_times} 现答题数 {now_times} 正确率 {now_right_rate * 100:.2f}%/{target_right_rate * 100}%')
         elif res_submit['message'] == '回答错误！': # 回答错误
             run_times += 1
             now_times += 1
@@ -206,11 +221,11 @@ def main():
                 sheet.append(data_list)
                 workbook.save(f'青马易战_{subjectId}.xlsx')
                 
-            print(f'# 本题回答错误！ 提交答案：{my_answer} 正确答案：{rightAnswer} 当前提交次数 {run_times}/{target_times} 现答对数 {now_right_times} 现答题数 {now_times} 正确率 {now_right_rate * 100:.2f}%/{target_right_rate * 100}%')
+            print(f'# 本题回答错误!  提交答案: {my_answer} 正确答案: {rightAnswer} 当前提交次数 {run_times}/{target_times} 现答对数 {now_right_times} 现答题数 {now_times} 正确率 {now_right_rate * 100:.2f}%/{target_right_rate * 100}%')
         elif res_submit['message'] == '您的答题速度过快，请认真答题，30s后可继续答题.': # 触发答题冷却
-            print('# 触发答题冷却，等待30s后自动继续..')
+            print('# 触发答题冷却, 等待30s后自动继续..')
             time.sleep(30)
-        else: # 发现错误，等待手动处理
+        else: # 发现错误, 等待手动处理
             print(f'# 发现未知回复文本: {res_submit}')
             input()
     print('# 运行完毕。')
@@ -220,4 +235,4 @@ if __name__ == '__main__':
         try:
             main()
         except Exception as e:
-            print('# 发现错误:', e)
+            print('# 发现错误: ', e)
